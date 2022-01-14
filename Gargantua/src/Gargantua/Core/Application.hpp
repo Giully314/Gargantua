52b69a9ff459478b01bf0ef0b76930f9a8c9fcb4
Giully314 <giully@protonmail.com>
2022-01-14 16:37:35 +0100

    New application interface

--- Gargantua/src/Gargantua/Core/Application.hpp
+++ Gargantua/src/Gargantua/Core/Application.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+
+#include "Gargantua/Time/TimeStep.hpp"
+
+
 namespace Gargantua
 {
 	namespace Core
@@ -20,10 +24,33 @@ namespace Gargantua
 
 			virtual ~Application() = default;
 
+
 			/*
-			Execute should perform a single "tick" of the app.
+			This function is called at the start of the Run method from Engine.
+			Note that this function doesn't replace the role of constructor.
 			*/
-			virtual void Execute(/*This function takes a TimeStep data*/) = 0;
+			virtual void Start() = 0;
+			
+			
+			/*
+			This function is called at the end of the Run method from Engine.
+			Note that this function doesn't replace the role of destructor.
+			*/
+			virtual void Shutdown() = 0;
+
+
+			/*
+			Reset the application. Is safe to call this function before Shutdown? 
+			Think about it
+			*/
+			virtual void Reset() = 0;
+
+
+			/*
+			Execute should perform a single "tick" of the app. A tick is a single update of the app with
+			a delta time.
+			*/
+			virtual void Execute(const Time::TimeStep&) = 0;
 		};
 
 
