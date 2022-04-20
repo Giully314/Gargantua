5b6c42e0cdfacf98467bf8f0d3ac92703f0740d1
Giully314 <giully@protonmail.com>
2022-04-20 23:13:54 +0200

    Entity typedef

---
+++ Gargantua/src/Gargantua/ECS/Types.hpp
@@ -0,0 +1,25 @@
+#pragma once
+/*
+Gargantua/ECS/Types.hpp
+
+
+PURPOSE:
+
+CLASSES:
+
+DESCRIPTION:
+
+USAGE:
+
+*/
+
+#include <cstdint>
+
+
+namespace Gargantua
+{
+	namespace ECS
+	{
+		using Entity = std::uint32_t;
+	} //namespace ECS
+} //namespace Gargantua
