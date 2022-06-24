a0ba87af3872bd2ed36193fb3ee6ab0a3354f44d
Giully314 <giully@protonmail.com>
2022-06-24 21:37:22 +0200

    Events for Dear ImGui

---
+++ Gargantua/src/Gargantua/Event/ImGuiEvents.cpp
@@ -0,0 +1,14 @@
+/*
+Gargantua/Event/ImGuiEvents.cpp
+*/
+#include "ImGuiEvents.hpp"
+
+
+namespace Gargantua
+{
+	namespace Event
+	{
+		const event_name_t ImGuiEvent::EventName{ "ImGuiEvent" };
+		const event_name_t PanelResizeEvent::EventName{ "PanelResizeEvent" };
+	} //namespace Event
+} //namespace Gargantua
