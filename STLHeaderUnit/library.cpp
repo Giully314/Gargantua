1b0093f83a0b08474e48603a653427d9a492742f
Giully314 <giully@protonmail.com>
2022-12-31 17:24:46 +0100

    added modules

---
+++ STLHeaderUnit/library.cpp
@@ -0,0 +1,62 @@
+/*
+STLHeaderUnit/library.cpp
+
+DESCRIPTION:
+	This file is used as a similar concept of precompiled header file but for modules.
+	For more informations: 
+	https://learn.microsoft.com/en-us/cpp/build/walkthrough-import-stl-header-units?view=msvc-170#approach1
+*/
+
+import <iostream>;
+import <fstream>;
+import <sstream>;
+
+import <string>;
+import <string_view>;
+import <format>;
+
+import <memory>;
+
+import <algorithm>;
+import <ranges>;
+
+import <type_traits>;
+import <concepts>;
+
+import <typeinfo>;
+import <typeindex>;
+
+import <iterator>;
+import <utility>;
+import <functional>;
+
+import <compare>;
+
+import <cmath>;
+import <numbers>;
+import <limits>;
+
+import <cstdint>;
+import <cstddef>;
+import <cstdlib>;
+import <cassert>;
+
+import <chrono>;
+
+import <source_location>;
+
+import <mutex>;
+import <condition_variable>;
+import <thread>;
+import <atomic>;
+import <future>;
+import <stop_token>;
+
+import <array>;
+import <vector>;
+import <unordered_map>;
+import <set>;
+import <deque>;
+import <list>;
+import <queue>;
+import <tuple>;
---
+++ STLHeaderUnit/premake5.lua
@@ -0,0 +1,20 @@
+project "STLHeaderUnit"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    systemversion "latest"
+    compileas "Module"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {"library.cpp"}
+
+    filter "configurations:Debug"
+      symbols "On"
+      buildoptions {"/MDd /EHsc /translateInclude" }
+
+
+   filter "configurations:Release"
+      optimize "On"
+      buildoptions {"/MD /EHsc /translateInclude"}
