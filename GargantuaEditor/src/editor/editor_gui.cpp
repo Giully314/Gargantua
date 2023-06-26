1ecda0727d73ed29c04453787930b4d1122fac00
Giully314 <giully@protonmail.com>
2023-06-26 14:18:35 +0200

    Changed aesthetics of the editor, adding and removing entities and modify components

--- GargantuaEditor/src/editor/editor_gui.cpp
+++ GargantuaEditor/src/editor/editor_gui.cpp
@@ -5,35 +5,115 @@
 module;
 
 #include <imgui.h>
+#include <imgui_internal.h>
 //#include <misc/cpp/imgui_stdlib.h>
 
 module gargantua.editor.editor_gui;
 
-#define MAX_TEXT_LEN 64
+import <ranges>;
+
+#define MAX_TEXT_LEN 24
 
 
 namespace gargantua::editor
 {
     // *********************************************** UTILITY FUNCTIONS IMGUI DRAW ******************************
     
-    static auto DrawFloat(std::string_view name, f32& f) -> void
+    static auto DrawFloatControl(std::string_view name, f32& f) -> void
     {
+        //ImGui::PushID(name.data());
+
         ImGui::DragFloat(name.data(), &f, 0.01f, -3.0f, 3.0f);
     }
     
-    static auto DrawVec3d(std::string_view name, math::Vec3df& v) -> void
+    static auto DrawVec3dControl(std::string_view name, math::Vec3df& v, 
+        f32 reset_value = 0.0f, f32 column_width = 100.0f) -> void
     {
-        ImGui::DragFloat3(name.data(), &v.x, 0.01f, -3.0f, 3.0f);
+        ImGuiIO& io = ImGui::GetIO();
+        auto bold_font = io.Fonts->Fonts[0];
+        
+        
+        // TEMPORARY, this avoid to have different vectors binded to the same "slide", because we assigned 
+        // ##X, ##Y, ##Z as names.
+        ImGui::PushID(name.data());
+
+        ImGui::Columns(2);
+        ImGui::SetColumnWidth(0, column_width);
+        ImGui::Text(name.data());
+        ImGui::NextColumn();
+
+        ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0 , 0 });
+
+        // From imgui
+        f32 line_height = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+        ImVec2 button_size = { line_height + 3.0f, line_height };
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.9f, 0.2f, 0.2f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
+        ImGui::PushFont(bold_font);
+        if (ImGui::Button("X", button_size))
+        {
+            v.x = reset_value;
+        }
+        ImGui::PopFont();
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        ImGui::DragFloat("##X", &v.x, 0.1f);
+        ImGui::PopItemWidth();
+        ImGui::SameLine();
+
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.3f, 0.8f, 0.3f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
+        ImGui::PushFont(bold_font);
+        if (ImGui::Button("Y", button_size))
+        {
+            v.y = reset_value;
+        }
+        ImGui::PopFont();
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        ImGui::DragFloat("##Y", &v.y, 0.1f);
+        ImGui::PopItemWidth();
+        ImGui::SameLine();
+
+
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.2f, 0.25f, 0.9f, 1.0f });
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
+        ImGui::PushFont(bold_font);
+        if (ImGui::Button("Z", button_size))
+        {
+            v.z = reset_value;
+        }
+        ImGui::PopFont();
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        ImGui::DragFloat("##Z", &v.z, 0.1f);
+        ImGui::PopItemWidth();
+        
+
+        ImGui::PopStyleVar();
+
+        ImGui::Columns(1);
+
+        ImGui::PopID();
+        //ImGui::DragFloat3(name.data(), &v.x, 0.01f, -3.0f, 3.0f);
     }
 
     static auto DrawText(std::string_view name, std::string& s) -> void
     {
         char text[MAX_TEXT_LEN] = {};
         s.copy(text, MAX_TEXT_LEN);
+        ImGui::PushItemWidth(MAX_TEXT_LEN * 5);
         if (ImGui::InputText(name.data(), text, MAX_TEXT_LEN))
         {
             s = text;
         }
+        ImGui::PopItemWidth();
 
         // Impossible to use with misc/..., error in building for conflicts with other libraries.
         //ImGui::InputText(name.data(), &s, ImGuiInputTextFlags_CallbackResize);
@@ -58,72 +138,172 @@ namespace gargantua::editor
 
         ImGuiIO& io = ImGui::GetIO();
 
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
+        //ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
         ImGui::Begin("Scene manager");
-        static ImGuiTreeNodeFlags base_flags = ImGuiTreeNodeFlags_OpenOnArrow | 
-            ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth;
+        /*static ImGuiTreeNodeFlags base_flags = ImGuiTreeNodeFlags_OpenOnArrow | 
+            ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth;*/
         
         if (ImGui::CollapsingHeader("Scene Hierarchy"))
         {
             EntityNode();
         }
-        ImGui::End();
-        ImGui::PopStyleVar();
 
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
+
+        // Right click on a blank space in scene manager open a popup.
+        if (ImGui::BeginPopupContextWindow(0, 1, false))
+        {
+            if (ImGui::MenuItem("Create empty entity"))
+            {
+                context->CreateEntity("Empty entity");
+            }
+
+            ImGui::EndPopup();
+        }
+
+
+        ImGui::End();
+        //ImGui::PopStyleVar();
+
+
+
+        //ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
         ImGui::Begin("Properties");
         
-        if (entity_selected != ecs::null_entity)
+        if (entity_selected.IsValid())
         {
             ShowComponents(entity_selected);
         }
 
         ImGui::End();
-        ImGui::PopStyleVar();
+        //ImGui::PopStyleVar();
 	}
 
 
     auto EditorGUI::EntityNode() -> void
     {
+        // We can't delete an entity inside the for each because the view would be not valid anymore.
+        // So we need to "save" the current entity to be deleted and do the job after the for each.
+        scene::Entity entity_to_be_deleted;
         if (ImGui::TreeNode("Entities"))
         {
             auto& ecs_s = context->ECS();
-            auto group = ecs_s.Group<scene::TagComponent>();
-            auto view = group.View();
-            ImGuiTreeNodeFlags node_flags = base_flags;
-            node_flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
-            for (auto e : view)
+            auto entities_view = context->GetEntities() | 
+                std::views::transform([&](auto e)
+                    {
+                        return scene::Entity{e, context};
+                    });
+            
+            
+            for (auto e : entities_view)
             {
-                auto& tag = ecs_s.Get<scene::TagComponent>(e);
-                ImGui::TreeNodeEx((void*)(e), node_flags, tag.name.c_str());
+                const ImGuiTreeNodeFlags node_flags = ((entity_selected == e) ? ImGuiTreeNodeFlags_Selected : 0) | 
+                    ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_SpanAvailWidth;
+                //node_flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
+
+                std::string_view tag;
+                if (e.Has<scene::TagComponent>())
+                {
+                    tag = e.Get<scene::TagComponent>().name;
+                }
+                else
+                {
+                    tag = "Entity";
+                }
+
+                bool opened = ImGui::TreeNodeEx((void*)(e.ID()), node_flags, tag.data());
+                
                 if (ImGui::IsItemClicked() && !ImGui::IsItemToggledOpen())
                 {
                     entity_selected = e;
                 }
+
+                bool entity_deleted = false;
+                if (ImGui::BeginPopupContextItem())
+                {
+                    if (ImGui::MenuItem("Delete entity"))
+                    {
+                        entity_to_be_deleted = e;
+                    }
+                    ImGui::EndPopup();
+                }
             }
 
             ImGui::TreePop();
+        
+            entity_to_be_deleted.Destroy();
         }
     }
 
 
-    auto EditorGUI::ShowComponents(ecs::entity_t e) -> void
+    auto EditorGUI::AddComponentNode() -> void
+    {
+        if (ImGui::MenuItem("Tag"))
+        {
+            entity_selected.Emplace<scene::TagComponent>();
+            ImGui::CloseCurrentPopup();
+        }
+        else if (ImGui::MenuItem("Transform"))
+        {
+            entity_selected.Emplace<render::TransformComponent>();
+            ImGui::CloseCurrentPopup();
+        }
+        else if (ImGui::MenuItem("Texture"))
+        {
+            entity_selected.Emplace<render::TextureComponent>();
+            ImGui::CloseCurrentPopup();
+        }
+    }
+
+
+    auto EditorGUI::ShowComponents(scene::Entity entity) -> void
     {
         using namespace render;
         using namespace scene;
 
-        auto entity = context->CreateEntityFromID(e);
+        //static ImGuiComboFlags flags = 0;
+        //ImGui::CheckboxFlags("ImGuiComboFlags_PopupAlignLeft", &flags, ImGuiComboFlags_PopupAlignLeft);
 
         ShowComponent<TagComponent>("Tag", entity, [](TagComponent& t)
             {
-                DrawText("TagName", t.name);
+                DrawText("##Tag", t.name);
             });
+
+
+        ImGui::SameLine();
+        ImGui::PushItemWidth(-1);
+
+        if (ImGui::Button("Add component"))
+        {
+            ImGui::OpenPopup("add_component");
+        }
+        if (ImGui::BeginPopup("add_component"))
+        {
+            AddComponentNode();
+            ImGui::EndPopup();
+        }
+
+        ImGui::PopItemWidth();
+
 
         ShowComponent<TransformComponent>("Transform", entity, [](TransformComponent& t)
             {
-                DrawVec3d("Position", t.position);
-                DrawFloat("Rotation", t.rotation);
-                DrawVec3d("Scale", t.scale);
+                DrawVec3dControl("Position", t.position);
+                DrawFloatControl("Rotation", t.rotation);
+                DrawVec3dControl("Scale", t.scale, 1.0f);
+            });
+
+        ShowComponent<TextureComponent>("Texture", entity, [=](TextureComponent& t)
+            {
+                // TODO: create a new texture with a name
+                // Can be loaded from a file or created using the color palette.
+
+                ImGui::ColorEdit4("Color", reinterpret_cast<f32*>(&t.color));
+                ImGui::DragFloat("Tiling factor", &t.tiling_factor, 0.1f, 0.0f, 100.0f);
+            });
+
+        ShowComponent<CameraComponent>("Camera", entity, [](CameraComponent& c)
+            {
+
             });
     }
 } // namespace gargantua::editor
--- GargantuaEditor/src/editor/editor_gui.ixx
+++ GargantuaEditor/src/editor/editor_gui.ixx
@@ -21,8 +21,11 @@ module;
 export module gargantua.editor.editor_gui;
 
 import <string_view>;
+import <string>;
 import <vector>;
 import <typeinfo>;
+import <array>;
+import <concepts>;
 
 import gargantua;
 
@@ -45,33 +48,34 @@ namespace gargantua::editor
 	private:
 		// TODO: concepts for types
 		template <typename T, typename F>
+			requires std::invocable<F, T&>
 		auto ShowComponent(std::string_view name, scene::Entity e, F&& f) -> void
 		{
-			const ImGuiTreeNodeFlags treeNodeFlags = ImGuiTreeNodeFlags_DefaultOpen | 
+			const ImGuiTreeNodeFlags tree_node_flags = ImGuiTreeNodeFlags_DefaultOpen | 
 				ImGuiTreeNodeFlags_Framed | ImGuiTreeNodeFlags_SpanAvailWidth | 
 				ImGuiTreeNodeFlags_AllowItemOverlap | ImGuiTreeNodeFlags_FramePadding;
 			if (e.Has<T>())
 			{
 				auto& component = e.Get<T>();
-				ImVec2 contentRegionAvailable = ImGui::GetContentRegionAvail();
+				ImVec2 content_region_avail = ImGui::GetContentRegionAvail();
 
 				ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2{ 4, 4 });
-				float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+				float line_height = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
 				ImGui::Separator();
-				bool open = ImGui::TreeNodeEx((void*)typeid(T).hash_code(), treeNodeFlags, name.data());
+				bool open = ImGui::TreeNodeEx((void*)typeid(T).hash_code(), tree_node_flags, name.data());
 				ImGui::PopStyleVar();
-				ImGui::SameLine(contentRegionAvailable.x - lineHeight * 0.5f);
-				if (ImGui::Button("+", ImVec2{ lineHeight, lineHeight }))
+				ImGui::SameLine(content_region_avail.x - line_height * 0.5f);
+				if (ImGui::Button("+", ImVec2{ line_height, line_height }))
 				{
-					ImGui::OpenPopup("ComponentSettings");
+					ImGui::OpenPopup("component_settings");
 				}
 				
 				// TODO: show better.
-				bool removeComponent = false;
-				if (ImGui::BeginPopup("ComponentSettings"))
+				bool remove_component = false;
+				if (ImGui::BeginPopup("component_settings"))
 				{
 					if (ImGui::MenuItem("Remove component"))
-						removeComponent = true;
+						remove_component = true;
 
 					ImGui::EndPopup();
 				}
@@ -82,24 +86,32 @@ namespace gargantua::editor
 					ImGui::TreePop();
 				}
 
-				if (removeComponent)
+				if (remove_component)
+				{
 					e.Erase<T>();
+				}
 			}
 		}
 
 
-		auto CameraNode() -> void;
 		auto EntityNode() -> void;
+		auto ShowComponents(scene::Entity e) -> void;
+		
 
+		auto AddComponentNode() -> void;
 
-		auto ShowComponents(ecs::entity_t e) -> void;
 
 	private:
 		non_owned_res<scene::SceneContext> context;
-		ecs::entity_t entity_selected = ecs::null_entity;
+		scene::Entity entity_selected;
 
 		const ImGuiTreeNodeFlags base_flags = ImGuiTreeNodeFlags_OpenOnArrow |
 			ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_SpanAvailWidth;
+	
+		
+		const std::array<const std::string_view, 4> component_names = {
+			"Tag", "Transform", "Camera", "Texture",
+		};
 	};
 } // namespace gargantua
 
