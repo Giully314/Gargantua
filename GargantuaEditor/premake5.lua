3818b5246fc678412912bcd771fb25d809273150
Giully314 <giully@protonmail.com>
2023-07-17 16:44:54 +0200

    premake

---
+++ GargantuaEditor/premake5.lua
@@ -0,0 +1,30 @@
+project "GargantuaEditor"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	systemversion "latest"
+
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {"src/**.ixx", "src/**.cpp"}
+
+	includedirs 
+	{
+		"%{wks.location}/Gargantua/src",
+		"%{wks.location}/Gargantua/vendor/imgui",
+		"%{wks.location}/Gargantua/vendor/Glad/include",
+	}
+
+	links {"Gargantua"}
+
+
+	filter "configurations:Debug"
+		symbols "On"
+		runtime "Debug"
+
+	filter "configurations:Release"
+		optimize "On"
+		runtime "Release"
+		--buildoptions "/MT"
